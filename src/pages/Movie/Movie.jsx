e792d1806766fb14cf2fa75555cc607e5d0d4a4b
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-01-24 16:50:26 -0300

    details resposiveness

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -1,7 +1,8 @@
 import styles from './Movie.module.css'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, CardMedia, Container, Grid, Typography } from '@mui/material'
+import { Box, CardMedia, Container, Grid, Typography, Card } from '@mui/material'
+import RatingStars from '../../components/RatingStars/RatingStars'
 
 const Movie = () => {
     const { id } = useParams()
@@ -36,42 +37,83 @@ const Movie = () => {
                 {movie && movie.title}
             </Typography>
 
-            <Grid
-                container
-                py={5}
-                spacing={5}
-                display='flex'
-                justifyContent='center'
-                alignItems='center'
-            >
+            {movie &&
                 <Grid
-                    item
-                    xs={12} sm={6} md={4} lg={3}
+                    container
+                    py={5}
+                    spacing={5}
                     display='flex'
-                    justifyContent='right'
+                    justifyContent='center'
+                    alignItems='center'
                 >
-                    {movie &&
-                        <CardMedia
-                            component="img"
-                            height="450"
-                            style={{ objectFit: 'contain', alignSelf: 'flex-start' }}
-                            image={imageURL + movie.poster_path}
-                            alt={movie.title}
-                        />
-                    }
-                </Grid>
+                    <Grid
+                        item
+                        xs={12} sm={6} md={4} lg={3}
+                        justifyContent='right'
+                        sx={{
+                            display: { xs: 'none', sm: 'flex' }
+                        }}
+                    >
+                        <Card
+                            height={450}
+                            sx={{
+                                position: 'relative'
+                            }}
+                        >
+                            <CardMedia
+                                sx={{
+                                    borderRadius: '10px',
+                                    boxShadow: '0 5px 10px black'
+                                }}
+                                component="img"
+                                image={imageURL + movie.poster_path}
+                                alt={movie.title}
+                            />
+                            {/* ICONE DE MUDAR IMAGEM */}
+                        </Card>
+                    </Grid>
 
-                <Grid
-                    item
-                    xs={12} sm={6} md={4} lg={3}
-                    display='flex'
-                    justifyContent='left'
-                >
-                    <Typography textAlign='center'>
-                        Teste
-                    </Typography>
+                    <Grid
+                        item
+                        xs={12} sm={6} md={4} lg={3}
+                    >
+                        <Box
+                            height={450}
+                            display='flex'
+                            flexDirection='column'
+                            justifyContent='space-between'
+                            alignItems='center'
+                        >
+                            <Box>
+                                <Typography>
+                                    budget
+                                </Typography>
+                            </Box>
+                            {/*
+                                genres
+                                overview
+                                production companies / countrys
+                                release date
+                                runtime
+                            */}
+
+                            <Box sx={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}>
+                                <RatingStars rate={movie.vote_average} />
+
+                                <Typography
+                                    variant='body2'
+                                    ml={1}
+                                >
+                                    ({movie.vote_count})
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
                 </Grid>
-            </Grid>
+            }
         </Container>
     )
 }
