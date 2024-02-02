b45425e33e62d5ea72ecafe41ec63810dc094ec1
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-01 21:43:46 -0300

    link updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -82,6 +82,10 @@ function Navbar() {
     const [search, setSearch] = useState('')
     const navigate = useNavigate()
 
+    const handlePortfolioNavigate = () => {
+        window.location.href = 'https://pedromarcelino7.github.io/Portfolio/'
+    }
+
     const handleSubmit = (e) => {
         if (e.key === 'Enter') {
             if (!search) return
